e0da7c0cd27fa099ceb5ec274bd7dfc076a4edeb
WeenGood <provotorov24@gmail.com>
2018-12-12 21:06:58 +0300

    lite version

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,190 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pathB = new System.Windows.Forms.Button();
+            this.pathT = new System.Windows.Forms.TextBox();
+            this.labA = new System.Windows.Forms.Label();
+            this.labB = new System.Windows.Forms.Label();
+            this.labC = new System.Windows.Forms.Label();
+            this.labD = new System.Windows.Forms.Label();
+            this.labX1 = new System.Windows.Forms.Label();
+            this.labX2 = new System.Windows.Forms.Label();
+            this.labX2C = new System.Windows.Forms.Label();
+            this.labX1C = new System.Windows.Forms.Label();
+            this.labDC = new System.Windows.Forms.Label();
+            this.labX2Res = new System.Windows.Forms.Label();
+            this.labX1Res = new System.Windows.Forms.Label();
+            this.labDRes = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // pathB
+            // 
+            this.pathB.Location = new System.Drawing.Point(565, 154);
+            this.pathB.Name = "pathB";
+            this.pathB.Size = new System.Drawing.Size(75, 23);
+            this.pathB.TabIndex = 0;
+            this.pathB.Text = "button1";
+            this.pathB.UseVisualStyleBackColor = true;
+            this.pathB.Click += new System.EventHandler(this.pathB_Click);
+            // 
+            // pathT
+            // 
+            this.pathT.Location = new System.Drawing.Point(268, 154);
+            this.pathT.Name = "pathT";
+            this.pathT.Size = new System.Drawing.Size(291, 20);
+            this.pathT.TabIndex = 1;
+            this.pathT.TextChanged += new System.EventHandler(this.pathT_TextChanged);
+            // 
+            // labA
+            // 
+            this.labA.AutoSize = true;
+            this.labA.Location = new System.Drawing.Point(25, 88);
+            this.labA.Name = "labA";
+            this.labA.Size = new System.Drawing.Size(28, 13);
+            this.labA.TabIndex = 2;
+            this.labA.Text = "labA";
+            // 
+            // labB
+            // 
+            this.labB.AutoSize = true;
+            this.labB.Location = new System.Drawing.Point(25, 112);
+            this.labB.Name = "labB";
+            this.labB.Size = new System.Drawing.Size(35, 13);
+            this.labB.TabIndex = 3;
+            this.labB.Text = "label2";
+            // 
+            // labC
+            // 
+            this.labC.AutoSize = true;
+            this.labC.Location = new System.Drawing.Point(25, 137);
+            this.labC.Name = "labC";
+            this.labC.Size = new System.Drawing.Size(35, 13);
+            this.labC.TabIndex = 4;
+            this.labC.Text = "label3";
+            // 
+            // labD
+            // 
+            this.labD.AutoSize = true;
+            this.labD.Location = new System.Drawing.Point(25, 168);
+            this.labD.Name = "labD";
+            this.labD.Size = new System.Drawing.Size(35, 13);
+            this.labD.TabIndex = 5;
+            this.labD.Text = "label4";
+            // 
+            // labX1
+            // 
+            this.labX1.AutoSize = true;
+            this.labX1.Location = new System.Drawing.Point(25, 194);
+            this.labX1.Name = "labX1";
+            this.labX1.Size = new System.Drawing.Size(35, 13);
+            this.labX1.TabIndex = 6;
+            this.labX1.Text = "label5";
+            // 
+            // labX2
+            // 
+            this.labX2.AutoSize = true;
+            this.labX2.Location = new System.Drawing.Point(25, 217);
+            this.labX2.Name = "labX2";
+            this.labX2.Size = new System.Drawing.Size(35, 13);
+            this.labX2.TabIndex = 7;
+            this.labX2.Text = "label6";
+            // 
+            // labX2C
+            // 
+            this.labX2C.AutoSize = true;
+            this.labX2C.Location = new System.Drawing.Point(78, 217);
+            this.labX2C.Name = "labX2C";
+            this.labX2C.Size = new System.Drawing.Size(35, 13);
+            this.labX2C.TabIndex = 13;
+            this.labX2C.Text = "label6";
+            // 
+            // labX1C
+            // 
+            this.labX1C.AutoSize = true;
+            this.labX1C.Location = new System.Drawing.Point(78, 194);
+            this.labX1C.Name = "labX1C";
+            this.labX1C.Size = new System.Drawing.Size(35, 13);
+            this.labX1C.TabIndex = 12;
+            this.labX1C.Text = "label5";
+            // 
+            // labDC
+            // 
+            this.labDC.AutoSize = true;
+            this.labDC.Location = new System.Drawing.Point(78, 168);
+            this.labDC.Name = "labDC";
+            this.labDC.Size = new System.Drawing.Size(35, 13);
+            this.labDC.TabIndex = 11;
+            this.labDC.Text = "label4";
+            // 
+            // labX2Res
+            // 
+            this.labX2Res.AutoSize = true;
+            this.labX2Res.Location = new System.Drawing.Point(129, 217);
+            this.labX2Res.Name = "labX2Res";
+            this.labX2Res.Size = new System.Drawing.Size(35, 13);
+            this.labX2Res.TabIndex = 16;
+            this.labX2Res.Text = "label6";
+            // 
+            // labX1Res
+            // 
+            this.labX1Res.AutoSize = true;
+            this.labX1Res.Location = new System.Drawing.Point(129, 194);
+            this.labX1Res.Name = "labX1Res";
+            this.labX1Res.Size = new System.Drawing.Size(35, 13);
+            this.labX1Res.TabIndex = 15;
+            this.labX1Res.Text = "label5";
+            // 
+            // labDRes
+            // 
+            this.labDRes.AutoSize = true;
+            this.labDRes.Location = new System.Drawing.Point(129, 168);
+            this.labDRes.Name = "labDRes";
+            this.labDRes.Size = new System.Drawing.Size(35, 13);
+            this.labDRes.TabIndex = 14;
+            this.labDRes.Text = "label4";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labX2Res);
+            this.Controls.Add(this.labX1Res);
+            this.Controls.Add(this.labDRes);
+            this.Controls.Add(this.labX2C);
+            this.Controls.Add(this.labX1C);
+            this.Controls.Add(this.labDC);
+            this.Controls.Add(this.labX2);
+            this.Controls.Add(this.labX1);
+            this.Controls.Add(this.labD);
+            this.Controls.Add(this.labC);
+            this.Controls.Add(this.labB);
+            this.Controls.Add(this.labA);
+            this.Controls.Add(this.pathT);
+            this.Controls.Add(this.pathB);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button pathB;
+        private System.Windows.Forms.TextBox pathT;
+        private System.Windows.Forms.Label labA;
+        private System.Windows.Forms.Label labB;
+        private System.Windows.Forms.Label labC;
+        private System.Windows.Forms.Label labD;
+        private System.Windows.Forms.Label labX1;
+        private System.Windows.Forms.Label labX2;
+        private System.Windows.Forms.Label labX2C;
+        private System.Windows.Forms.Label labX1C;
+        private System.Windows.Forms.Label labDC;
+        private System.Windows.Forms.Label labX2Res;
+        private System.Windows.Forms.Label labX1Res;
+        private System.Windows.Forms.Label labDRes;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Globalization;
 
 namespace diplom
 {
@@ -16,5 +19,193 @@ namespace diplom
         {
             InitializeComponent();
         }
+
+        private void pathB_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ok = new OpenFileDialog();
+
+            if (ok.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                pathT.Text = ok.FileName;
+            }
+
+            ok.Dispose();
+        }
+
+        private void pathT_TextChanged(object sender, EventArgs e)
+        {
+
+            if (pathT.Text.Substring(pathT.Text.LastIndexOf(".") + 1) == "xmcd")
+            {
+                FileStream myStream = new FileStream(pathT.Text, FileMode.Open);
+                StreamReader myReader = new StreamReader(myStream);
+
+                string mcdStr;
+
+                do
+                {
+                    mcdStr = myReader.ReadLine();
+                    if (mcdStr == null)
+                    {
+                        MessageBox.Show("Ошибка!", "В файле не найдена переменная numA");
+                        break;
+                    }
+                } while (mcdStr.IndexOf("numA") == -1);
+
+                mcdStr = myReader.ReadLine();
+                float numA = retNum(mcdStr);
+
+                myReader.Close();
+                myStream.Close();
+                myStream.Dispose();
+
+                myStream = new FileStream(pathT.Text, FileMode.Open);
+                myReader = new StreamReader(myStream);
+                do
+                {
+                    mcdStr = myReader.ReadLine();
+                    if (mcdStr == null)
+                    {
+                        MessageBox.Show("Ошибка!", "В файле не найдена переменная numB");
+                        break;
+                    }
+                } while (mcdStr.IndexOf("numB") == -1);
+
+                mcdStr = myReader.ReadLine();
+                float numB = retNum(mcdStr);
+
+                myReader.Close();
+                myStream.Close();
+                myStream.Dispose();
+                myStream = new FileStream(pathT.Text, FileMode.Open);
+                myReader = new StreamReader(myStream);
+                do
+                {
+                    mcdStr = myReader.ReadLine();
+                    if (mcdStr == null)
+                    {
+                        MessageBox.Show("Ошибка!", "В файле не найдена переменная numC");
+                        break;
+                    }
+                } while (mcdStr.IndexOf("numC") == -1);
+
+                mcdStr = myReader.ReadLine();
+                float numC = retNum(mcdStr);
+
+                myReader.Close();
+                myStream.Close();
+                myStream.Dispose();
+
+                myStream = new FileStream(pathT.Text, FileMode.Open);
+                myReader = new StreamReader(myStream);
+                do
+                {
+                    mcdStr = myReader.ReadLine();
+                    if (mcdStr == null)
+                    {
+                        MessageBox.Show("Ошибка!", "В файле не найдена переменная discr");
+                        break;
+                    }
+                } while (mcdStr.IndexOf("discr") == -1);
+
+                mcdStr = myReader.ReadLine();
+                float discr = retNum(mcdStr);
+
+                myReader.Close();
+                myStream.Close();
+                myStream.Dispose();
+
+                myStream = new FileStream(pathT.Text, FileMode.Open);
+                myReader = new StreamReader(myStream);
+                do
+                {
+                    mcdStr = myReader.ReadLine();
+                    if (mcdStr == null)
+                    {
+                        MessageBox.Show("Ошибка!", "В файле не найдена переменная x1");
+                        break;
+                    }
+                } while (mcdStr.IndexOf("x1") == -1);
+
+                mcdStr = myReader.ReadLine();
+                float x1 = retNum(mcdStr);
+
+                myReader.Close();
+                myStream.Close();
+                myStream.Dispose();
+
+                myStream = new FileStream(pathT.Text, FileMode.Open);
+                myReader = new StreamReader(myStream);
+                do
+                {
+                    mcdStr = myReader.ReadLine();
+                    if (mcdStr == null)
+                    {
+                        MessageBox.Show("Ошибка!", "В файле не найдена переменная x2");
+                        break;
+                    }
+                } while (mcdStr.IndexOf("x2") == -1);
+
+                mcdStr = myReader.ReadLine();
+                float x2 = retNum(mcdStr);
+
+                myReader.Close();
+                myStream.Close();
+                myStream.Dispose();
+
+                float myDiscr = (float)Math.Round(Convert.ToDouble(numB * numB - 4 * numA * numC), 3);
+
+
+                float myX1 = (float)Math.Round(Convert.ToDouble(-numB - Math.Sqrt(myDiscr)) / (2 * numA), 3);
+                float myX2 = (float)Math.Round(Convert.ToDouble(-numB + Math.Sqrt(myDiscr)) / (2 * numA), 3);
+
+                labA.Text = "a = " + numA.ToString();
+                labB.Text = "b = " + numB.ToString();
+                labC.Text = "c = " + numC.ToString();
+                labD.Text = "D = " + discr.ToString();
+                labDC.Text = "D' = " + myDiscr.ToString();
+                labX1.Text = "x1 = " + x1.ToString();
+                labX2.Text = "x2 = " + x2.ToString();
+                labX1C.Text = "x1' = " + myX1.ToString();
+                labX2C.Text = "x2' = " + myX2.ToString();
+                if (discr == myDiscr)
+                    labDRes.Text = "+";
+                else labDRes.Text = "-";
+                if (x1 == myX1 || x2==myX1)
+                    labX1Res.Text = "+";
+                else labX1Res.Text = "-";
+                if (x2 == myX2 || x1==myX2)
+                    labX2Res.Text = "+";
+                else labX2Res.Text = "-";
+            }
+            else MessageBox.Show("Ошибка!", "Неправильный формат файла!");
+
+        }
+        public float retNum(string str)
+        {
+            float num = 0;
+            int i = 0;
+
+            foreach (var a in str)
+                if (Char.IsNumber(a) || a == '-')
+                {
+                    i = str.IndexOf(a);
+                    break;
+                }
+
+            string numStr = "";
+
+            do
+            {
+                numStr += str[i];
+                i++;
+            } while (Char.IsNumber(str[i]) || str[i] == '.');
+
+
+
+            return num = float.Parse(numStr, CultureInfo.InvariantCulture.NumberFormat);
+        }
     }
 }
+
+
